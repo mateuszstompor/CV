6c1d97106287278ad9ff91021c8c5fdd9a360f10
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-10-16 00:18:28 +0200

    Prepare CV in latex

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash -e
+
+#
+#  Copyright © 2022 Mateusz Stompór. All rights reserved.
+#
+
+DIST_DIR=distribution
+BUILD_DATE=$(TZ=Europe/Warsaw date +"%m%d%H%M")
+AUTHOR_NAME="Mateusz Stompór"
+TITLE="CV"
+FILE_NAME="${AUTHOR_NAME} - ${TITLE} - ${BUILD_DATE}"
+
+fail() {
+  echo $1
+  exit 1
+}
+
+mkdir -p ${DIST_DIR} || fail "Could not create output directories"
+pdflatex -halt-on-error \
+         -jobname="${FILE_NAME}" \
+         -output-directory=${DIST_DIR} \
+         mateuszstompor.tex 2>&1 1>/dev/null || fail "Could not build the pdf"
